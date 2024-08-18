adfda9f490f968590c18e066b9ec7fa110e5c922
Daria Grishina <dd.dgrishinad@gmail.com>
2024-08-18 14:07:07 +0300

    Fix

--- conftest.py
+++ conftest.py
@@ -2,22 +2,16 @@ import pytest
 from selenium import webdriver
 
 
-# @pytest.fixture(params=['chrome', 'firefox'])
-# def driver(request):
-#     if request.param == 'chrome':
-#         browser = webdriver.Chrome()
-#         browser.maximize_window()
-#     elif request.param == 'firefox':
-#         browser = webdriver.Firefox()
-#         browser.maximize_window()
-#     else:
-#         raise ValueError('Unknown browser')
-#     yield browser
-#     browser.quit()
-
-@pytest.fixture
+@pytest.fixture(params=['chrome', 'firefox'])
 def driver(request):
-    browser = webdriver.Chrome()
-    browser.maximize_window()
+    if request.param == 'chrome':
+        browser = webdriver.Chrome()
+        browser.maximize_window()
+    elif request.param == 'firefox':
+        browser = webdriver.Firefox()
+        browser.maximize_window()
+    else:
+        raise ValueError('Unknown browser')
     yield browser
     browser.quit()
+
